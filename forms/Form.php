9ee809de92956c353cf92ead66d7e10e8c91fe85
Glitch (tart-cemetery) <none>
2019-07-23 15:13:45 +0000

    👖🍜 Checkpoint ./forms/Form.php:86128/4720

--- forms/Form.php
+++ forms/Form.php
@@ -66,11 +66,89 @@ class Form implements ArrayAccess {
   }
   
   public static function required($key, $value) {
-    return strlen(trim($value)) ? false : self::fieldToHuman($key) . " is required";
+    $pattern = '/[[:graph:]]+/';
+    if (!preg_match($pattern, $value)) {
+      return self::fieldToHuman($key) . " is required";
+    }
+    return false;
+  }
+  
+  public static function phone($key, $value) {
+    $pattern = "/^\(?\d{3}\)?[. -]?\d{3}[. -]?\d{4}$/";
+    if (!preg_match($pattern, $value)) {
+      return "$value is not a valid phone number";
+    }
+    return false;
+  }
+
+  public static function integer($key, $value) {
+    $pattern = '/^\d+$/';
+    if (!preg_match($pattern, $value)) {
+      return $value . " is not a valid " . self::fieldToHuman($key);
+    }
+    return false;
+  }
+  
+  public static function float($key, $value) {
+    $pattern = '/^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$/';
+    if (!preg_match($pattern, $value)) {
+      return $value . " is not a valid " . self::fieldToHuman($key);
+    }
+    return false;
+  }
+  
+  public static function money($key, $value) {
+    $pattern = '/^\$?\d+([.]?\d{2})?$/';
+    if (!preg_match($pattern, $value)) {
+      return $value . " is not a valid " . self::fieldToHuman($key);
+    }
+    return false;
   }
   
   public static function password($key, $value) {
-    return strlen(trim($value)) >= 8 ? false : self::fieldToHuman($key) . " must be 8 or more characters";
+    $patterns = array(
+      '/^[[:graph:]]{8,}$/',  # all printable (no ws) and at least 8 in length
+      '/[[:upper:]]/',        # at least 1 upper case character
+      '/[[:digit:]]/',        # at least 1 digit
+      '/[[:punct:]]/'         # at least 1 symbol
+    );
+    foreach ($patterns as $pattern) {
+      if (!preg_match($pattern, $value)) {
+        return self::fieldToHuman($key) . ' must be at least 8 characters, contain an upper case, digit, and symbol.'
+      }
+    }
+    return false;
+  }
+
+  public static function email($key, $value, $message = false) {
+    // this regex copied from
+    // http://fightingforalostcause.net/misc/2006/compare-email-regex.php
+    $pattern = '/^(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|' . 
+    '(?:\x22?[^\x5C\x22]\x22?)){255,})(?!(?:(?:\x22?\x5C' .
+    '[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){65,}@)' .
+    '(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)' .
+    '|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]' .
+    '|(?:\x5C[\x00-\x7F]))*\x22))(?:\.(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-' .
+    '\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-' .
+    '\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22)))*@(?:(?:(?!.*[^.]{64,})' .
+    '(?:(?:(?:xn--)?[a-z0-9]+(?:-[a-z0-9]+)*\.){1,126}){1,}' .
+    '(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-[a-z0-9]+)*)' .
+    '|(?:\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})' .
+    '|(?:(?!(?:.*[a-f0-9][:\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?' .
+    '::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}' .
+    '(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}' .
+    '(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?' .
+    '(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))' .
+    '(?:\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9])))' .
+    '{3}))\]))$/iD';
+    if (!preg_match($pattern, $value)) {
+      return $value . " is not a valid email address";
+    }
+    return false;
+  }
+  
+  public static function same($key, $value1, $value2) {
+    
   }
   
   public function offsetExists($offset) {
