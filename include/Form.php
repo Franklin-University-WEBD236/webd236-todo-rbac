fef7995beaf7b7a157f246eea459f345131569ee
Glitch (tart-cemetery) <none>
2019-07-23 00:31:47 +0000

    🖼🐣 Checkpoint ./include/Form.php:86128/860

--- include/Form.php
+++ include/Form.php
@@ -1,13 +1,33 @@
 <?php
 class Form {
+
+  private $data;
+  private $validators;
   
-  public __construct() {
-    $this->values = array();
-    $this->
+  public function __construct($fields = array()) {
+    foreach ($fields as $field => $validators) {
+      $this->addField($key, $validator);
+    }
   }
   
-  public addField($key, $validators) {
+  public function addField($key, $validators) {
+    $this->data[$key] = null;
     $this->validators[$key] = $validators;
+  }
+  
+  public function validate() {
+    foreach ($this->validators as $key => $validators) {
+      foreach ($validators as $validator) {
+        $result = $validator($this->data[$key]);
+        if ($result) {
+          addError($key, $result);
+        }
+      }
+    }
+  }
+  
+  private function addError($message) {
+    if ()
   }
 }
 ?>
