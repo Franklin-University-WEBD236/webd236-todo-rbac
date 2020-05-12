9e0fa4632988048526b067a8636fc7961415e29c
Glitch (tart-cemetery) <none>
2020-05-12 18:59:23 +0000

    🌚🚩 Checkpoint ./models/PermissionModel.php:86128/4632

--- views/cache_AdminIndex.php
+++ views/cache_AdminIndex.php
@@ -99,11 +99,7 @@
 <div class="row">
   <div class="col-lg-12">
     <h1><?php echo(htmlspecialchars($title)); ?></h1>
-    <p>This is an example program that models the principles and
-    practices taught in WEBD 236 at <a href="https://franklin.edu">
-    Franklin University</a> in Columbus, Ohio. This course was
-    developed by <a href="https://www.franklin.edu/about-us/faculty-staff/faculty-profiles/whittakt">
-    Todd Whittaker</a> and is copyrighted &copy; by Franklin University.</p>
+    <p>Replace this with your view contents</p>
   </div>
 </div>
           
