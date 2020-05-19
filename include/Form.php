f112ba551f716906eff96a530501cb8be83804ae
Glitch (tart-cemetery) <none>
2020-05-19 14:42:01 +0000

    👚⛸ Checkpoint ./include/Form.php:86128/1613

--- include/Form.php
+++ include/Form.php
@@ -4,19 +4,49 @@ class Form {
   private $fields;
   
   public function __construct() {
-    $this -> fields = array();
+    $this -> fields = [];
     
   }
   
-  public function textbox($name) {
-    
+  public function field($name, $type, $label, $valid="", $options=[]) {
+    $this->fields[$name] = [
+      "type" => $type,
+      "label" => $label,
+      "valid" => $valid,
+      "options" => $options,
+    ]
   }
   
   public function toHtml() {
+    $result = "";
+    $temp = "";
+    foreach ($fields as $name => $params) {
+      switch ($params['type']) {
+        case "text" : $temp = _text($name, $params['label'], $params['options']); break;
+      }
+      $result .= $temp;
+    }
+    return result;
 //      <div class="form-group">
 //        <label for="email">Email address</label>
 //        <input type="text" min="1" id="email" name="form[email]" class="form-control"
 // placeholder="Enter email address" value="{{$this->value($form['email'])}}" />
 //      </div>
   }
+  
+  private function _text($name, $label, $options) {
+    $opts = [
+      "class" => "form-control",
+      "placeholder" => $label,
+    ];
+    array_merge($opts, $options);
+    return <<<END
+    <div class="form-group">
+      <label for="$name">$label</label>
+      <input type="text" min="1" id="$name" name="form[$name]" class="form-control" placeholder="$label" />
+    </div>
+    END;
+  }
+  
+  private function _build()
 }
