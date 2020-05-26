566790320ebcb48d5b82ccf9afbdd8bfccc00c72
Glitch (tart-cemetery) <none>
2020-05-26 21:35:34 +0000

    🐾🎍 Checkpoint ./views/AdminIndex.php:86128/6

--- views/AdminIndex.php
+++ views/AdminIndex.php
@@ -41,7 +41,7 @@
           <td class="align-middle"><?php echo "{$group -> getName()}" ?></td>
           <td>
             <div class="btn-toolbar align-middle float-right">
-              <button class="btn btn-success d-flex justify-content-center align-content-between mr-1" onclick="get('@@admin/edit_members/{{$group -> id}}@@')"><span class="material-icons">group</span></button>
+              <button class="btn btn-success d-flex justify-content-center align-content-between mr-1" onclick="get('@@admin/edit_group_members/{{$group -> id}}@@')"><span class="material-icons">group</span></button>
               <button class="btn btn-primary d-flex justify-content-center align-content-between mr-1" onclick="get('@@admin/edit_group/{{$group -> id}}@@')"><span class="material-icons">create</span></button>
               <button class="btn btn-danger d-flex justify-content-center align-content-between" onclick="post('@@admin/delete_group/{{$group -> id}}@@')"><span class="material-icons">delete</span></button>
             </div>
--- views/cache_AdminIndex.php
+++ views/cache_AdminIndex.php
@@ -141,7 +141,7 @@
           <td class="align-middle"><?php echo "{$group -> getName()}" ?></td>
           <td>
             <div class="btn-toolbar align-middle float-right">
-              <button class="btn btn-success d-flex justify-content-center align-content-between mr-1" onclick="get('/admin/edit_members/<?php echo(htmlspecialchars($group -> id)); ?>')"><span class="material-icons">group</span></button>
+              <button class="btn btn-success d-flex justify-content-center align-content-between mr-1" onclick="get('/admin/edit_group_members/<?php echo(htmlspecialchars($group -> id)); ?>')"><span class="material-icons">group</span></button>
               <button class="btn btn-primary d-flex justify-content-center align-content-between mr-1" onclick="get('/admin/edit_group/<?php echo(htmlspecialchars($group -> id)); ?>')"><span class="material-icons">create</span></button>
               <button class="btn btn-danger d-flex justify-content-center align-content-between" onclick="post('/admin/delete_group/<?php echo(htmlspecialchars($group -> id)); ?>')"><span class="material-icons">delete</span></button>
             </div>
